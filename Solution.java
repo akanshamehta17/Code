19ba5aa6f82267768561facf91228c9f4f0101bd
Akansha Mehta <akansha.mehta@sjsu.edu>
2017-11-24 21:37:56 -0800

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -8,7 +8,7 @@ Given nums = [2, 7, 11, 15], target = 9,
 Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
 
-//code
+//code with time complexity O(n^2)
 class Solution {
     int[] nums;
     int[] nums2 = new int[2];
@@ -27,6 +27,50 @@ class Solution {
                     flag = true;
                 }
                 
+            }
+        }
+        return nums2;
+    }
+    
+    public static void main(String[] args){
+        int[] nums = {3,2,4};
+        Solution s = new Solution();
+        s.twoSum(nums,6);
+    }
+}
+
+//code with time complexity O(n)
+class Solution {
+    int[] nums;
+    int[] nums2;
+    //int[] nums2 = new int[2];
+    int target;
+    //boolean flag = false;
+    
+    public int[] twoSum(int[] nums, int target) {
+        /*for(int i=0;i<nums.length && !flag;i++){
+            for(int j=1;j<nums.length && !flag;j++){
+                if(i==j) continue;
+                else if(nums[i]+nums[j] == target){
+                    for(int k=0;k<1;k++){
+                        nums2[k] = i;
+                        nums2[k+1] = j;
+                    }
+                    flag = true;
+                }
+                
+            }
+        }
+        return nums2;*/
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for(int i=0;i<nums.length;i++){
+            map.put(nums[i],i);
+        }
+        
+        for(int j=0;j<nums.length;j++){
+            int val = target - nums[j];
+            if(map.containsKey(val) && map.get(val)!=j){
+                nums2 = new int[]{map.get(val),j};
             }
         }
         return nums2;
