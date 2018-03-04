f6226bc37a4ada6f75c6f9b4a22b34d8b707a1f4
Akansha Mehta <akansha.mehta@sjsu.edu>
2018-03-03 19:37:55 -0800

    Create Solution.java

---
+++ Search Insert Position/Solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        
+        int result = -1;
+        
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==target){
+                result = i;
+                break;
+            }
+                
+            else if(nums[i]>target){
+                result = i;
+                    break;
+            }
+        
+                
+        }
+        
+        if(result==-1){
+            result = nums.length;
+        }
+        
+        return result;
+        
+    }
+}
